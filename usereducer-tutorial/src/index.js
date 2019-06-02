61b00c5518cf06cf8cbbce954a792ca151d1c82a
Sem8 <mssemmi8@gmail.com>
2019-06-02 18:20:10 -0400

    added clear all functionality and prioritize one shopping item functionality using useReducer

--- usereducer-tutorial/src/index.js
+++ usereducer-tutorial/src/index.js
@@ -14,8 +14,16 @@ function ShoppingList() {
             name: action.name
           }
         ];
-        case 'remove':
-            return state.filter((_, index) => index != action.index)
+
+      case "remove":
+        return state.filter((_, index) => index != action.index);
+
+      case "clear":
+        return [];
+
+      case "prioritize":
+        return state.filter((item, index) => index === action.index);
+
       default:
         return state;
     }
@@ -23,10 +31,12 @@ function ShoppingList() {
 
   function handleSubmit(event) {
     event.preventDefault();
+
     dispatch({
       type: "add",
       name: inputRef.current.value
     });
+    
     inputRef.current.value = "";
   }
 
@@ -36,12 +46,16 @@ function ShoppingList() {
         <input ref={inputRef} />
       </form>
 
+      <button onClick={() => dispatch({ type: "clear" })}>Clear</button>
       <ul>
         {items.map((item, index) => (
           <li key={item.id}>
             {item.name}
             <button onClick={() => dispatch({ type: "remove", index })}>
               X
+            </button>
+            <button onClick={() => dispatch({ type: "prioritize", index })}>
+              Prioritize
             </button>
           </li>
         ))}
