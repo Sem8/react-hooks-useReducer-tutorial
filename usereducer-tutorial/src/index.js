406622ea025e06da5d6fb065434bfc8b4a5f0ce4
Sem8 <mssemmi8@gmail.com>
2019-06-02 17:04:46 -0400

    added increment count functionality with click of a button using useReducer

--- usereducer-tutorial/src/index.js
+++ usereducer-tutorial/src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Count() {
+    const [count, dispatch] = useReducer((state, action) => {
+        //...
+        if (action === 'add') {
+            return state + 1;
+        }
+        return state;
+    }, 0)
+    return (
+        <div>{count}<button onClick={() => dispatch('add')}>Increment</button></div>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+ReactDOM.render(<Count />, document.getElementById('root'));
+
