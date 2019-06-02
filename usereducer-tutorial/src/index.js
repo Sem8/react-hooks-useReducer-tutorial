848693a900e665145e13b10ceb6239ac27247d8d
Sem8 <mssemmi8@gmail.com>
2019-06-02 17:14:22 -0400

    added decrement functionality using useReducer

--- usereducer-tutorial/src/index.js
+++ usereducer-tutorial/src/index.js
@@ -1,20 +1,25 @@
-import React, { useReducer } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useReducer } from "react";
+import ReactDOM from "react-dom";
 // import App from './App';
 
 function Count() {
-    const [count, dispatch] = useReducer((state, action) => {
-        //...
-        if (action === 'add') {
-            return state + 1;
-        }
+  const [count, dispatch] = useReducer((state, action) => {
+    switch (action) {
+      case "add":
+        return state + 1;
+      case "-":
+        return state - 1;
+      default:
         return state;
-    }, 0)
-    return (
-        <div>{count}<button onClick={() => dispatch('add')}>Increment</button></div>
-    )
+    }
+  }, 0);
+  return (
+    <div>
+      {count}
+      <button onClick={() => dispatch("add")}>Increment</button>
+      <button onClick={() => dispatch("-")}>Decrement</button>
+    </div>
+  );
 }
 
-
-ReactDOM.render(<Count />, document.getElementById('root'));
-
+ReactDOM.render(<Count />, document.getElementById("root"));
